d3e58aae325fdfc194b00876d7ce27671fd5ef15
Diego <mi@email.com>
2024-08-09 10:25:32 +0200

    Primer commit proyecto vacio

---
+++ src/main/java/es/cic/ejercicio07/Ejercicio07Application.java
@@ -0,0 +1,13 @@
+package es.cic.ejercicio07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicio07Application.class, args);
+	}
+
+}
